8feca69c9fee950e81115eb1ba66b63d723e0303
zlk <849693228@qq.com>
2019-04-02 00:14:47 +0800

    111

--- src/server/data/internal/module_guanka.go
+++ src/server/data/internal/module_guanka.go
@@ -5,23 +5,28 @@ import (
 	"server/tool"
 )
 
+// AllChapters ...
 func (m *Module) AllChapters(player *entry.Player) []*entry.Chapter {
 	if player == nil || len(player.UserId) == 0 {
 		return nil
 	}
 	if m.playerChapters[player.UserId] == nil {
 		chapters := make([]*entry.Chapter, 0)
-		ch := m.FindChapterDefine(1)
-		chapter := new(entry.Chapter)
-		tool.DeepCopy(chapter, ch)
-		chapter.IsOpen = true
-		chapters = append(chapters, chapter)
+		for i, ch := range m.chapters {
+			chapter := new(entry.Chapter)
+			tool.DeepCopy(chapter, ch)
+			if i == 0 {
+				chapter.IsOpen = true
+			}
+			chapters = append(chapters, chapter)
+		}
 		m.playerChapters[player.UserId] = chapters
 	}
 
 	return m.playerChapters[player.UserId]
 }
 
+// AllGuanKas ...
 func (m *Module) AllGuanKas(player *entry.Player) []*entry.GuanKa {
 	if player == nil || len(player.UserId) == 0 {
 		return nil
@@ -39,29 +44,32 @@ func (m *Module) AllGuanKas(player *entry.Player) []*entry.GuanKa {
 	return m.playerGuanKas[player.UserId]
 }
 
-func (m *Module) FindChapterDefine(chapterId int32) *entry.Chapter {
+// FindChapterDefine ...
+func (m *Module) FindChapterDefine(chapterID int32) *entry.Chapter {
 	for _, chapter := range m.chapters {
-		if chapter.Id == chapterId {
+		if chapter.Id == chapterID {
 			return chapter
 		}
 	}
 	return nil
 }
 
-func (m *Module) FindGuanKaDefine(guanKaId int32) *entry.GuanKa {
+// FindGuanKaDefine ...
+func (m *Module) FindGuanKaDefine(guanKaID int32) *entry.GuanKa {
 	for _, guanKa := range m.guanKas {
-		if guanKa.Id == guanKaId {
+		if guanKa.Id == guanKaID {
 			return guanKa
 		}
 	}
 	return nil
 }
 
-func (m *Module) FindGuanKa(player *entry.Player, guanKaId int32) *entry.GuanKa {
+// FindGuanKa ...
+func (m *Module) FindGuanKa(player *entry.Player, guanKaID int32) *entry.GuanKa {
 	guanKas := m.AllGuanKas(player)
 	if guanKas != nil {
 		for _, guanKa := range guanKas {
-			if guanKa.Id == guanKaId {
+			if guanKa.Id == guanKaID {
 				return guanKa
 			}
 		}
